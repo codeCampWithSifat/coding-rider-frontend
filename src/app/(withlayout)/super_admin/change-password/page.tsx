34149db2d9f868f07dce6d122be5f9136f176ea0
codeCampWithSifat <sayedhossainsifat300@.com>
2023-11-13 01:09:43 +0600

    module 52 just started

--- src/app/(withlayout)/super_admin/change-password/page.tsx
+++ src/app/(withlayout)/super_admin/change-password/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { Button } from "antd";
+
 const ResetPassPage = () => {
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      <h2>This page is for change password </h2>
+    <div
+      style={{ margin: "100px 0", display: "flex", justifyContent: "center" }}
+    >
+      <Form submitHandler={onSubmit}>
+        <h3 style={{ marginBottom: "10px" }}>Reset Password</h3>
+        <div style={{ margin: "5px 0" }}>
+          <FormInput name="oldPassword" label="Old password" type="password" />
+        </div>
+        <div style={{ margin: "5px 0" }}>
+          <FormInput name="newPassword" label="New password" type="password" />
+        </div>
+        <Button type="primary" htmlType="submit" style={{ margin: "1rem 0" }}>
+          submit
+        </Button>
+      </Form>
     </div>
   );
 };
